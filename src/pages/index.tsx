f6a290fd04fdbd9a2b38879ab2c2b972ea65ab29
yamatohagi <la.luce.ymt0326@gmail.com>
2023-06-11 16:42:36 +0900

    文言

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <>
       <Head>
-        <title>The starting point for your next project | Climb Bond</title>
+        <title>壁屋の集い | Climb Bond</title>
       </Head>
 
       <ClimberPostsView />

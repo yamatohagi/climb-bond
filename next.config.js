ab6fae8195205ab036d065c2ee84dd4b7e3df99a
yamatohagi <la.luce.ymt0326@gmail.com>
2023-06-16 22:06:25 +0900

    これで行けるか

--- next.config.js
+++ next.config.js
@@ -4,17 +4,4 @@ module.exports = {
   experimental: {
     scrollRestoration: true,
   },
-  async headers() {
-    return [
-      {
-        // Matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'https://climbbond.com' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,PUT,POST,DELETE,PATCH' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
-        ],
-      },
-    ];
-  },
 };
--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,6 +1,8 @@
+// /src/pages/api/graphql.ts
 import 'reflect-metadata';
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import allowCors from 'src/lib/cros';
 import { schema } from '../../../prisma/scheme';
 import { prisma } from '../../../prisma/context';
 
@@ -8,6 +10,8 @@ const server = new ApolloServer({
   schema,
 });
 
-export default startServerAndCreateNextHandler(server, {
+const handler = startServerAndCreateNextHandler(server, {
   context: async (req) => ({ req, prisma }),
 });
+
+export default allowCors(handler); // <-- apply the middleware
